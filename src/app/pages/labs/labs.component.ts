9e0981db3107d28987f446a65efed441d93e1b29
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-11-23 19:34:28 +0100

    update lab component

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-labs',
-  imports: [],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
-
+  title = 'todoapp';
+  welcome = 'todoapp';
+  tasks = ['Task 1', 'Task 2', 'Task 3'];
 }
