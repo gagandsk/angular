a67ca6838ec18fc810aa6f1b766e98d56c46f466
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-11-27 17:14:31 +0100

    Eventos de Teclado en JavaScript: Uso de KeyDown y Change

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -31,4 +31,9 @@ export class LabsComponent {
   changeHandler(event: Event) {
     console.log('---->', event);
   }
+
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log('Input value:', input.value);
+  }
 }
